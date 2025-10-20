13eda1d335d31ababfc13b0d1489d3aa48008fc3
Amir Mahla <amir.mahla@icloud.com>
2025-10-20 15:49:33 +0200

    REMOVE unused code

--- cua2-front/src/component/poc/index.ts
+++
@@ -1,8 +0,0 @@
-export { ConnectionStatus } from './ConnectionStatus';
-export { Header } from './Header';
-export { Metadata } from './Metadata';
-export { ProcessingIndicator } from './ProcessingIndicator';
-export { StackSteps } from './StackSteps';
-export { StepCard } from './StepCard';
-export { TaskButton } from './TaskButton';
-export { VNCStream } from './VNCStream';
